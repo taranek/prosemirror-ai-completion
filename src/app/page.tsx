ef2b415c554ca27b716b9aa85902b52fa8390a8c
taranek <t.taranek27@gmail.com>
2025-08-02 14:38:14 +0200

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import { CompletionExplainer } from "@/components/CompletionExplainer";
 
 export default function Home() {
 	return (
-		<div className="font-sans flex justify-center items-start justify-items-center min-h-[100dvh] p-8 pb-20 gap-16 sm:p-20">
+		<div className="font-sans flex justify-center items-start justify-items-center p-8 pb-20 gap-16 sm:p-20">
 			<main className="flex flex-col gap-5 row-start-2 items-center sm:items-start w-full max-w-[900px]">
 				<div className="w-full flex justify-between items-start">
 					<div>
-						<h1 className="text-4xl font-bold">Prosemirror AI assistant</h1>
+						<h1 className="text-4xl font-bold mb-2">Prosemirror AI assistant</h1>
 						<p className="mb-2 text-foreground-50">
 							Use AI to assist with your Prosemirror editor. Type something in the
 							editor and wait for the AI to complete your thoughts.
