abce947abbea2d22127401bc20a21046f8dcfba8
taranek <t.taranek27@gmail.com>
2025-08-02 14:40:02 +0200

    update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { CompletionExplainer } from "@/components/CompletionExplainer";
 
 export default function Home() {
 	return (
-		<div className="font-sans flex justify-center items-start justify-items-center p-8 pb-20 gap-16 sm:p-20">
+		<div className="font-sans flex justify-center items-start justify-items-center h-full p-8 pb-20 gap-16 sm:p-20">
 			<main className="flex flex-col gap-5 row-start-2 items-center sm:items-start w-full max-w-[900px]">
 				<div className="w-full flex justify-between items-start">
 					<div>
